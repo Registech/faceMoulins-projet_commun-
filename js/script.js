03044602f9fe5392bb775375b6424d100597585d
MARTINEZ Kévin <msi03@laposte.net>
2017-02-01 23:39:45 +0100

    liens_effectifs

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-var img1 = document.getElementById('img1')
-var article1 = document.getElementById('articleUn')
-var article2 = document.getElementById('articleDeux')
-var article3 = document.getElementById('articleTrois')
-
-
-
-article1.addEventListener('click',function(){
-	img1.src = "https://facegrandlyondotcom.files.wordpress.com/2014/05/capture-das-transparent.png";
+$("#articleUn").click(function(e){
+	e.preventDefault()
+	$("#img1").attr("src", "https://facegrandlyondotcom.files.wordpress.com/2014/05/capture-das-transparent.png");
 });
 
-article2.addEventListener('click',function(e){
-	e.preventEvent();
-	img1.src = "http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg";
+$("#articleDeux").click(function(e){
+	e.preventDefault()
+	$("#img1").attr("src", "http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg");
+});
+$("#articleTrois").click(function(e){
+	e.preventDefault()
+	$("#img1").attr("src", "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
 });
 
-article3.addEventListener('click',function(){
-	img1.src = "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png";
-});
+
