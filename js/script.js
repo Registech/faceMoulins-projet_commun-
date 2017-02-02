f62ba9fdd0bee1b55e84ad3e6ac92c2a1aca656b
Andrea <andrea-lpz@outlook.com>
2017-02-02 08:57:05 +0100

    modif jQuery

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-var img1 = document.getElementById('img1')
-var article1 = document.getElementById('articleUn')
-var article2 = document.getElementById('articleDeux')
-var article3 = document.getElementById('articleTrois')
+  ///////////////////////////////////
+ //----------CHANGE IMG-----------//
+///////////////////////////////////
+$('#articleUn').on('click', function(e){
+	e.preventDefault();
+	$('#img1').css('width', '550px');
+	$('#img1').css('height', '400px');
+	$('#img1').attr('src', 'https://facegrandlyondotcom.files.wordpress.com/2014/05/capture-das-transparent.png');
+})
+$("#articleDeux").on('click', function(e){
+	e.preventDefault();
+	$('#img1').css('width', '775px');
+	$('#img1').css('height', '400px');
+	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg');
+})
+$('#articleTrois').on('click',function(e){
+	e.preventDefault();
+	$('#img1').css('width', '775px');
+	$('#img1').css('height', '400px');
+	$('#img1').attr('src', "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
+})
 
 
 
-article1.addEventListener('click',function(){
-	img1.src = "https://facegrandlyondotcom.files.wordpress.com/2014/05/capture-das-transparent.png";
-});
-
-article2.addEventListener('click',function(e){
-	e.preventEvent();
-	img1.src = "http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg";
-});
-
-article3.addEventListener('click',function(){
-	img1.src = "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png";
-});
