29decb7c3f260cf9d1801c5e36df87cff889850b
Andrea <andrea-lpz@outlook.com>
2017-02-05 22:40:46 +0100

    imgJava

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ $('#articleUn').on('click', function(e){
 });
 $("#articleDeux").on('click', function(e){
 	e.preventDefault();
-	$('#img1').css('width', '775px');
+	$('#img1').css('width', '700px');
 	$('#img1').css('height', '400px');
 	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg');
 });
 $('#articleTrois').on('click',function(e){
 	e.preventDefault();
-	$('#img1').css('width', '775px');
+	$('#img1').css('width', '675px');
 	$('#img1').css('height', '400px');
 	$('#img1').attr('src', "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
 });
