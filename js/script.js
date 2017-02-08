4ad2c6eeaedaf73303849af0f01eb351f05df170
MARTINEZ Kévin <msi03@laposte.net>
2017-02-08 15:58:21 +0100

    page_Accueil_brut

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@
 ///////////////////////////////////
 $('#articleUn').on('click', function(e){
 	e.preventDefault();
-	$('#img1').css('width', '550px');
-	$('#img1').css('height', '400px');
-	$('#img1').attr('src', 'https://facegrandlyondotcom.files.wordpress.com/2014/05/capture-das-transparent.png');
+	$('#img1').css('width', '100%');
+	$('#img1').css('height', '100%');
+	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg');
 });
 $("#articleDeux").on('click', function(e){
 	e.preventDefault();
-	$('#img1').css('width', '700px');
-	$('#img1').css('height', '400px');
-	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg');
+	$('#img1').css('width', '100%');
+	$('#img1').css('height', '100%');
+	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/quartiers-banniere-675x385.png');
 });
 $('#articleTrois').on('click',function(e){
 	e.preventDefault();
-	$('#img1').css('width', '675px');
-	$('#img1').css('height', '400px');
+	$('#img1').css('width', '100%');
+	$('#img1').css('height', '100%');
 	$('#img1').attr('src', "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
 });
 
