80afa6f408258bd9f75ed62c10d9637c5833e4aa
Lenny Robol <Lenny Robol>
2017-02-09 09:42:13 +0100

    javaEtCss

--- js/script.js
+++ js/script.js
@@ -20,5 +20,23 @@ $('#articleTrois').on('click',function(e){
 	$('#img1').attr('src', "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
 });
 
-
+///////////////////////////////////
+ //----------Cache header---------//
+///////////////////////////////////
+$(document).on('scroll', function(){
+    $('#headerRecherche').stop().slideUp();
+    //$('#corpPage').css('margin-top', '-120px'); //voir avec animate et une durée de transition
+    if ($(this).scrollTop()<50){
+        $('#headerRecherche').stop().slideDown();
+    }
+});
+///////////////////////////////////
+ //----------Cache header---------//
+///////////////////////////////////
+$(document).on('scroll', function(){
+    $('#headerRecherche').stop().slideUp();
+    if ($(this).scrollTop()<50){
+        $('#headerRecherche').stop().slideDown();
+    }
+});
 
