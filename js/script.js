8d435100e6fa713b0bbd353e5f991044db2158e5
MARTINEZ Kévin <msi03@laposte.net>
2017-02-09 09:42:36 +0100

    mobileFirst

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ $('#articleUn').on('click', function(e){
 	$('#img1').css('width', '100%');
 	$('#img1').css('height', '100%');
 	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/hollande-675x385.jpg');
+	$("#lienArticle").attr("href", "http://www.fondationface.org/lancement-de-la-fondation-pour-linvestissement-social-et-le-developpement-humain/");
 });
 $("#articleDeux").on('click', function(e){
 	e.preventDefault();
 	$('#img1').css('width', '100%');
 	$('#img1').css('height', '100%');
 	$('#img1').attr('src', 'http://www.fondationface.org/wp-content/uploads/quartiers-banniere-675x385.png');
+	$("#lienArticle").attr("href", "http://www.fondationface.org/lancement-du-concours-sengager-pour-les-quartiers-edition-2017/");
 });
 $('#articleTrois').on('click',function(e){
 	e.preventDefault();
 	$('#img1').css('width', '100%');
 	$('#img1').css('height', '100%');
 	$('#img1').attr('src', "http://www.fondationface.org/wp-content/uploads/selfigaz-min-675x385.png");
+	$("#lienArticle").attr("href", "http://www.fondationface.org/civigaz-vainqueur-kioz-de-grdf/");
 });
 
 
