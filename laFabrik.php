9df88e70e42177c673926d6bc9a6bbbdc7f89c7a
Darell <alex03000@live.com>
2017-02-19 20:51:08 +0100

    page fabrik en cours de construction

--- laFabrik.php
+++ laFabrik.php
@@ -16,6 +16,40 @@
 		<div class="container-fluid" style="padding: 0">
 			<?php include("include/header.php"); ?>
 			<div class="container">
+				<div class="row top">
+				<h1 class="col-md-7 boxtitle apprenants"><i class="fa fa-users" aria-hidden="true"></i>LES 23 APPRENANTS</h1>
+				<h1 class="col-md-offset-1 col-md-4 boxtitle"><i class="fa fa-code" aria-hidden="true"></i>PRÉSENTATION</h1>
+					<div class="col-md-7 box">
+						<ol>
+							<li>Stephane Bariller <a href="https://github.com/MrSnakie"> Github</a><a href=""> Linkedin</a></li>
+							<li>Kevin Martinez <a href="https://github.com/BobGizzmo"> Github</a><a href=""> Linkedin</a></li>
+							<li>Alexandre Leroux <a href="https://github.com/Darell82"> Github</a><a href=""> Linkedin</a></li>
+							<li>William Le Gal <a href="https://github.com/will03000"> Github</a><a href=""> Linkedin</a></li>
+							<li>Martin Dagostino <a href="https://github.com/Extreeeme"> Github</a><a href=""> Linkedin</a></li>
+							<li>Emilie Leterne <a href="https://github.com/celebalane"> Github</a><a href=""> Linkedin</a></li>
+							<li>Yves Charvis <a href="https://github.com/YvesCharvis"> Github</a><a href=""> Linkedin</a></li>
+							<li>Eddy Huleu <a href="https://github.com/EddyHuleu"> Github</a><a href=""> Linkedin</a></li>
+							<li>Aude Echer <a href="https://github.com/Tsukihub"> Github</a><a href=""> Linkedin</a></li>
+							<li>Sopie Leber <a href="https://github.com/SophieElodie"> Github</a><a href=""> Linkedin</a></li>
+							<li>Andréa Lopez <a href="https://github.com/Andrealpz"> Github</a><a href=""> Linkedin</a></li>
+							<li>Regis Toupens <a href="https://github.com/Registech"> Github</a><a href=""> Linkedin</a></li>
+							<li>Gaël Truffard <a href="https://github.com/truffard"> Github</a><a href=""> Linkedin</a></li>
+							<li>Thomas Millien <a href="https://github.com/ThomasMR03"> Github</a><a href=""> Linkedin</a></li>
+							<li>Aymeric Lacoste <a href="https://github.com/aymeric-l"> Github</a><a href=""> Linkedin</a></li>
+							<li>Vladislav Abbatchi <a href="https://github.com/Abbatchi"> Github</a><a href=""> Linkedin</a></li>
+							<li>Vincent Chambonnier <a href="https://github.com/Vincent03"> Github</a><a href=""> Linkedin</a></li>
+							<li>Quentin Grau <a href="https://github.com/Quentin777"> Github</a><a href=""> Linkedin</a></li>
+							<li>Quentin Ansion <a href="https://github.com/Seizuko"> Github</a><a href=""> Linkedin</a></li>
+							<li>Mickaël Durand <a href="https://github.com/Sethisfir"> Github</a><a href=""> Linkedin</a></li>
+							<li>Sebastien Dutripon <a href="https://github.com/Sebastien03"> Github</a><a href=""> Linkedin</a></li>
+							<li>Athony Favier <a href="https://github.com/aFAVIER"> Github</a><a href=""> Linkedin</a></li>
+							<li>Hasna Chaddou <a href="https://github.com/hasnachad"> Github</a><a href=""> Linkedin</a></li>
+						</ol>
+					</div>
+					<div class="col-md-offset-1 col-md-4 box">
+								sdgqsgq
+					</div>
+				</div>
 			</div>
 			<?php include("include/footer.php"); ?>
 		</div>			
