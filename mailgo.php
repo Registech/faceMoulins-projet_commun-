2375b417ff3e99958f2236e9e30331262e8dd01e
MARTINEZ Kévin <msi03@laposte.net>
2017-02-22 06:45:21 +0100

    NewsLetter:OK_Contact:OK_bugAffichageH2Acceuil:OK

---
+++ mailgo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Gudea" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+		<title>Face-Moulins.fr</title>
+	</head>
+	<body>
+		<?php include("include/header.php"); ?>
+		<section style="height: 500px;" class="container">
+			<div id="mailenvoyé" class="text-center">
+				<h2 style="line-height: 600px;">Votre message à a bien été envoyé</h2>
+			</div>
+		</section>		
+
+		<?php include("include/footer.php"); ?>
+	</body>
+</html>
