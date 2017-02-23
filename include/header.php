f5fec24f606a184fb25428b2ffa5a31f82b96f5a
MrSnakie <petitloulou96@free.fr>
2017-02-23 16:21:41 +0100

    Design bar navigation mobile first

--- include/header.php
+++ include/header.php
@@ -1,76 +1,73 @@
 <!-- Header -->
 <header class="navbar navbar-fixed-top">
 
-				<!-- Logo & recherche -->
-				<div id="top">
-					<img src="img/faceimg.png" alt="logo_face" />
-					<div class="search hidden-xs hidden-sm">
-						<input type="text" name="search" placeholder="Recherche..." id="search">
-						<span><i class="fa fa-search"></i></span>
-					</div>
-				</div>
+	<!-- Logo & recherche -->
+	<div id="top">
+		<img src="img/faceimg.png" alt="logo_face" />
+		<div class="search hidden-xs hidden-sm">
+			<input type="text" name="search" placeholder="Recherche..." id="search">
+			<span><i class="fa fa-search"></i></span>
+		</div>
+	</div>
 
-				<!-- Menu / Navigation -->
-				<div id="bottom" class="row">
-					<!-- Ordinateur -->
-					<div class="container hidden-xs hidden-sm">
-						<ul class="nav navbar-nav">
-							<li class="col-md-2 text-center dropdown">
-								<a href="index.php" title="Accueil" class="active">Accueil</a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Presse</a></li>
-										<li><a href="#">Agenda</a></li>
-										<li><a href="#">Fil d'actualité</a></li>
-										<li><a href="#">Réseaux sociaux</a></li>
-									</ul>
-							</li>
-							<li class="col-md-2 text-center dropdown">
-								<a href="presentation.php" title="Présentation" class="active">Présentation</a>
-									<ul class="dropdown-menu">
-										<li><a href="presentation.php#histoire">Histoire</a></li>
-										<li><a href="presentation.php#actions">Actions</a></li>
-									</ul>
-							</li>
-							<li class="col-md-2 text-center dropdown">
-								<a href="galerie.php" title="Galerie" class="active">Galerie</a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Photos</a></li>
-										<li><a href="#">Médias</a></li>
-									</ul>
-							</li>
-							<li class="col-md-2 text-center dropdown">
-								<a href="partenariat.php" title="Partenariat" class="active">Partenariat</a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Entreprises</a></li>
-										<li><a href="#">Associations</a></li>
-										<li><a href="#">Particuliers</a></li>
-										<li><a href="#">Institutions</a></li>
-									</ul>
-							</li>
-							<li class="col-md-2 text-center dropdown">
-								<a href="laFabrik.php" title="Fabrik" class="active">La Fabrik</a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Photos</a></li>
-										<li><a href="#">Projets</a></li>
-										<li><a href="#">Actualités</a></li>
-										<li><a href="#">Présentation</a></li>
-									</ul>
-							</li>
-							<li class="col-md-2 text-center"><a href="contact.php" title="Contact" class="active">Contact</a></li>
-						</ul>
-					</div>
+	<!-- Menu / Navigation -->
+	<div id="bottom" class="row">
 
-					<!-- Mobile / Tablette -->
-					<div class="dropdown visible-xs visible-sm col-xs-12" style="box-shadow: none !important;">
-						<div class="Accueil dropdown-toggle text-center" id="MenuXS" data-toggle="dropdown">MENU</div>
-						<ul id="ulMenu" class="dropdown-menu">
-							<li class="Accueil col-md-2 text-center liMenu"><a href="index.php" title="Accueil" class="lienMenu">Accueil</a></li>
-							<li class="Accueil col-md-2 text-center liMenu2"><a href="#" title="Présentation" class="lienMenu">présentation</a></li>
-							<li class="Accueil col-md-2 text-center liMenu2"><a href="galerie.php" title="Gallerie" class="lienMenu">galerie</a></li>
-							<li class="Accueil col-md-2 text-center liMenu2"><a href="#" title="Partenariat" class="lienMenu">partenariat</a></li>
-							<li class="Accueil col-md-2 text-center liMenu2"><a href="#" title="La Fabrik" class="lienMenu">La Fabrik</a></li>
-							<li class="Accueil col-md-2 text-center liMenu2"><a href="#" title="Contact" class="lienMenu contact">Contact</a></li>
-						</ul>
-					</div>	
-				</div>
-			</header>
+		<!-- Ordinateur -->
+		<div class="container hidden-xs hidden-sm">
+			<ul class="nav navbar-nav">
+				<li class="col-md-2 text-center dropdown">
+					<a href="index.php" title="Accueil" class="active">Accueil</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Presse</a></li>
+						<li><a href="#">Agenda</a></li>
+						<li><a href="#">Fil d'actualité</a></li>
+						<li><a href="#">Réseaux sociaux</a></li>
+					</ul>
+				</li>
+				<li class="col-md-2 text-center dropdown">
+					<a href="presentation.php" title="Présentation" class="active">Présentation</a>
+					<ul class="dropdown-menu">
+						<li><a href="presentation.php#histoire">Histoire</a></li>
+						<li><a href="presentation.php#actions">Actions</a></li>
+					</ul>
+				</li>
+				<li class="col-md-2 text-center dropdown">
+					<a href="galerie.php" title="Galerie" class="active">Galerie</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Photos</a></li>
+						<li><a href="#">Médias</a></li>
+					</ul>
+				</li>
+				<li class="col-md-2 text-center dropdown">
+					<a href="partenariat.php" title="Partenariat" class="active">Partenariat</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Entreprises</a></li>
+						<li><a href="#">Associations</a></li>
+						<li><a href="#">Particuliers</a></li>
+						<li><a href="#">Institutions</a></li>
+					</ul>
+				</li>
+				<li class="col-md-2 text-center dropdown">
+					<a href="laFabrik.php" title="Fabrik" class="active">La Fabrik</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Photos</a></li>
+						<li><a href="#">Projets</a></li>
+						<li><a href="#">Actualités</a></li>
+						<li><a href="#">Présentation</a></li>
+					</ul>
+				</li>
+				<li class="col-md-2 text-center"><a href="contact.php" title="Contact" class="active">Contact</a></li>
+			</ul>
+		</div>
+
+		<!-- Mobile / Tablette -->
+		<div class="container hidden-md hidden-lg">
+			<span class="text">Navigation</span>
+			<div class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bars"></i></div>
+				<ul class="dropdown-menu">
+					<li class="text-center"><a href="index.php" title="Accueil" class="lienMenu">Accueil</a></li>
+				</ul>
+			</div>
+		</div>
+</header>
