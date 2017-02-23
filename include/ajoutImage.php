5b916ca4c88bb530eaff3f42086368381228dde6
Registech <registech@laposte.net>
2017-02-23 10:35:17 +0100

    ajoutImg

---
+++ include/ajoutImage.php
@@ -0,0 +1,6 @@
+<div class= "connexion">
+	<h3>Ajout image</h3>
+	<form method="POST" target="include/ajoutImage.php">
+	    <input type="file" name="Image" id="image" placeholder="charger images" />
+	    <input type="submit" name="submit" value="Enregistrer" />
+</div>
