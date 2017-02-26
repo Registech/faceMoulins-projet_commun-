0943ae383fba07938662302c40a618bf31b822e2
MrSnakie <petitloulou96@free.fr>
2017-02-26 23:59:04 +0100

    Fix mobile first nav & style

--- include/header.php
+++ include/header.php
@@ -12,62 +12,60 @@
 
 	<!-- Menu / Navigation -->
 	<div id="bottom" class="row">
+		<div class="container">
+			<nav class="navbar-header">
+				<span class="text navbar-toggle">Navigation</span>
+				<span class="bars navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<i class="fa fa-bars"></i>
+				</span>
+			</nav>
 
-		<!-- Ordinateur -->
-		<div class="container hidden-xs hidden-sm">
-			<ul class="nav navbar-nav">
-				<li class="col-md-2 text-center dropdown">
-					<a href="index.php" title="Accueil" class="active">Accueil</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Presse</a></li>
-						<li><a href="#">Agenda</a></li>
-						<li><a href="#">Fil d'actualité</a></li>
-						<li><a href="#">Réseaux sociaux</a></li>
-					</ul>
-				</li>
-				<li class="col-md-2 text-center dropdown">
-					<a href="presentation.php" title="Présentation" class="active">Présentation</a>
-					<ul class="dropdown-menu">
-						<li><a href="presentation.php#histoire">Histoire</a></li>
-						<li><a href="presentation.php#actions">Actions</a></li>
-					</ul>
-				</li>
-				<li class="col-md-2 text-center dropdown">
-					<a href="galerie.php" title="Galerie" class="active">Galerie</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Photos</a></li>
-						<li><a href="#">Médias</a></li>
-					</ul>
-				</li>
-				<li class="col-md-2 text-center dropdown">
-					<a href="partenariat.php" title="Partenariat" class="active">Partenariat</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Entreprises</a></li>
-						<li><a href="#">Associations</a></li>
-						<li><a href="#">Particuliers</a></li>
-						<li><a href="#">Institutions</a></li>
-					</ul>
-				</li>
-				<li class="col-md-2 text-center dropdown">
-					<a href="laFabrik.php" title="Fabrik" class="active">La Fabrik</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Photos</a></li>
-						<li><a href="#">Projets</a></li>
-						<li><a href="#">Actualités</a></li>
-						<li><a href="#">Présentation</a></li>
-					</ul>
-				</li>
-				<li class="col-md-2 text-center"><a href="contact.php" title="Contact" class="active">Contact</a></li>
-			</ul>
-		</div>
-
-		<!-- Mobile / Tablette -->
-		<div class="container hidden-md hidden-lg">
-			<span class="text">Navigation</span>
-			<div class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bars"></i></div>
-				<ul class="dropdown-menu">
-					<li><a href="index.php" title="Accueil">Accueil</a></li>
+			<nav id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+						<a href="index.php" title="Accueil" class="active">Accueil</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Presse</a></li>
+							<li><a href="#">Agenda</a></li>
+							<li><a href="#">Fil d'actualité</a></li>
+							<li><a href="#">Réseaux sociaux</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="presentation.php" title="Présentation" class="active">Présentation</a>
+						<ul class="dropdown-menu">
+							<li><a href="presentation.php#histoire">Histoire</a></li>
+							<li><a href="presentation.php#actions">Actions</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="galerie.php" title="Galerie" class="active">Galerie</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Photos</a></li>
+							<li><a href="#">Médias</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="partenariat.php" title="Partenariat" class="active">Partenariat</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Entreprises</a></li>
+							<li><a href="#">Associations</a></li>
+							<li><a href="#">Particuliers</a></li>
+							<li><a href="#">Institutions</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="laFabrik.php" title="Fabrik" class="active">La Fabrik</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Photos</a></li>
+							<li><a href="#">Projets</a></li>
+							<li><a href="#">Actualités</a></li>
+							<li><a href="#">Présentation</a></li>
+						</ul>
+					</li>
+					<li><a href="contact.php" title="Contact" class="active">Contact</a></li>
 				</ul>
-			</div>
+			</nav>
 		</div>
+	</div>
 </header>
--- js/script.js
+++ js/script.js
@@ -69,11 +69,11 @@ $(document).on('scroll', function(){
 $(document).ready(function(){
 	$(".dropdown").hover(
 		function() {
-			$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown(300);
 			$(this).toggleClass('open');
 		},
 		function() {
-			$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+			$('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp(300);
 			$(this).toggleClass('open');
 		}
 	);
