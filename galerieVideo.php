0532c11c12b84c61350177be3e715039885f5b3f
Darell <alex03000@live.com>
2017-02-27 09:03:51 +0100

    changement de la video

--- galerieVideo.php
+++ galerieVideo.php
@@ -14,7 +14,7 @@
 	<body>
 		<?php include("include/header.php");?>
 		<div class="container" id='container2'>
-			<iframe src="https://quik.gopro.com/v/UXE34ag0lU/" width="50%" height="100%">La fabrik, école du numérique fondée par la fondation FACE Territoire Bourbonnais</iframe>
+			<iframe width="500" height="500" src="https://www.youtube.com/embed/5Bj1752rKNk" frameborder="0" allowfullscreen></iframe> 
 		</div>
 		<?php include("include/footer.php");?>
 
