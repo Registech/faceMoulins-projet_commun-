10fbbf81715211d8ffb6d92c03f5231d2897a0cf
Vladislav Legoshin <abbatchi@gmail.com>
2017-03-07 10:51:41 +0100

    corrections galerY

--- demo.php
+++ demo.php
@@ -1,18 +1,10 @@
-<?php
-
-// Hiding notices:
-error_reporting(E_ALL^E_NOTICE);
-
-?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>galery face</title>
 
-<link rel="stylesheet" type="text/css" href="css/demo.css" />
+<link rel="stylesheet" type="text/css" href="css/css/demo.css" />
 <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/ui-darkness/jquery-ui.css" type="text/css" media="all" />
 
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
