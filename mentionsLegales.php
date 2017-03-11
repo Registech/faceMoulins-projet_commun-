048a3bba734ce0f742e65ae05c5338a1d5b97d5e
Darell <alex03000@live.com>
2017-03-11 21:48:49 +0100

    mention legale remasterisé

--- mentionsLegales.php
+++ mentionsLegales.php
@@ -4,10 +4,12 @@ session_start();
 	?>
 	<div class="container">
       <div class="row">
-<!-- <div class="col-xs-0 col-sm-0 col-md-1 col-lg-1"></div> -->
-        <div class="col-md-10 col col-md-offset-1" id="padding50">
-          <h1 class="h1page" id="padding"><span>M</span>entions légales</h1> <br>
-            <p>Propriétaire du site : FACE Territoire bourbonnais</p> <br>
+<div class="col-xs-0 col-sm-0 col-md-1 col-lg-1"></div>
+          <h1 class="col-md-12 boxtitle" id="padding"><span>M</span>entions légales</h1>
+        <div class=" box col-md-10 col col-md-offset-1" id="padding50">
+        <h2>Edition</h2>
+        <hR>
+            <p>Propriétaire du site : FACE Territoire bourbonnais</p>
             <p> Siège Social : 1 Rue Berthelot – 03000 MOULINS </p> 
             <p> Tél. : 01 49 22 68 68 </p> 
             <p> SIREN :814 390 894 </p> 
@@ -16,38 +18,42 @@ session_start();
             <p> Rédaction et mise à jour : Aymeric Lacoste </p> 
             <p> Hébergement du site : L'ordinateur de Aymeric – 1 rue du sel – 03000 Moulins </p>
             <p> Réalisation du site : Simplon.co http://face-territoire.co/ </p> <br>
-            <p> Informations nominatives </p> <br>
+            <h2> Informations nominatives </h2>
+            <hR>
             <p> Comme l’indique l’ article 34 de la loi « Informatique et Libertés » du 6 janvier 1978, toute personne dispose d’un droit d’accès, de modification, de rectification et de suppression des données qui la concerne. </p> 
             <p> Pour exercer ce droit, adressez-vous à : </p> 
             <p> Fondation FACE</p> 
             <p> 1 Rue Berthelot </p> 
             <p> 03000 MOULINS – FRANCE </p> <br>
 
-texte a definir : 
 
-            <p> Propriété intellectuelle </p> <br>
+            <h2>Propriété intellectuelle</h2>
+            <hR>
 
             <p> Le Site est la propriété exclusive de la Fondation FACE, seule habilitée à utiliser et exploiter les droits de propriété intellectuelle et droits de la personnalité attachés à ce site, notamment les marques, dessins et modèles, droits d’auteur et droit à l’image, à titre originaire ou par l’effet d’une cession ou d’une licence d’exploitation. </p> <br>
             <p> L’usage de tout ou partie du site www.fondationface.org, notamment par téléchargement, reproduction, transmission ou représentation à d’autres fins que pour usage personnel et privé dans un but non commercial est strictement interdit. Pour utiliser les visuels et/ou certains textes présents sur le site, merci d’en faire la demande via le formulaire de contact. </p> 
 
-            <p> Liens hypertextes vers d’autres sites </p> <br>
+            <p> Liens hypertextes vers d’autres sites </p>
 
-            <p> La création de liens hypertextes vers le Site ne peut être faite qu’avec l’autorisation écrite et préalable de la Fondation FACE qui pourra être révoquée à tout moment. La Fondation FACE, décline toute responsabilité concernant le contenu des sites liés au Site. </p> <br>
+            <p> La création de liens hypertextes vers le Site ne peut être faite qu’avec l’autorisation écrite et préalable de la Fondation FACE qui pourra être révoquée à tout moment. La Fondation FACE, décline toute responsabilité concernant le contenu des sites liés au Site. </p>
 
-            <p> Responsabilité de la Fondation FACE </p> <br>
+            <h2>Responsabilité de la Fondation FACE</h2>
+            <hR>
 
             <p> Dans les conditions autorisées par la loi, la Fondation FACE n’encourt aucune responsabilité : </p> 
+            <ul>
+                <li>Pour toute imprécision, inexactitude, omission ou pour tous dommages résultant d’une intrusion d’un tiers ayant entraîné une modification des informations mises à disposition sur le Site, </li> 
 
-            <p> – Pour toute imprécision, inexactitude, omission ou pour tous dommages résultant d’une intrusion d’un tiers ayant entraîné une modification des informations mises à disposition sur le Site, </p> 
+                <li>En cas de dommages provoqués à raison de l’accès d’un tiers non autorisé sur le Site www.fondationface.org ou rendant impossible son accès. </li>
 
-            <p> – En cas de dommages provoqués à raison de l’accès d’un tiers non autorisé sur le Site www.fondationface.org ou rendant impossible son accès. </p>
+                <li>Le visiteur est informé que l’accès au Site pourra être interrompu à tout moment par la Fondation FACE pour des raisons de maintenance, de sécurité ou toute autre contrainte technique. </li>
+            </ul> 
 
-            <p> Le visiteur est informé que l’accès au Site pourra être interrompu à tout moment par la Fondation FACE pour des raisons de maintenance, de sécurité ou toute autre contrainte technique. </p> <br> 
+            <h2> Droit applicable </h2>
+            <hR>
 
-            <p> Droit applicable </p> <br>
-
-            <p> Les présentes conditions sont régies par le droit français. Les juridictions françaises sont seules compétentes pour connaître de tout litige se rapportant directement ou indirectement à l’accès au présent Site ou à son utilisation.</p> <br>
-<!--       <div class="col-xs-0 col-sm-0 col-md-1 col-lg-1"></div> -->
+            <p> Les présentes conditions sont régies par le droit français. Les juridictions françaises sont seules compétentes pour connaître de tout litige se rapportant directement ou indirectement à l’accès au présent Site ou à son utilisation.</p>
+<div class="col-xs-0 col-sm-0 col-md-1 col-lg-1"></div>
           </div> 
        </div>          
     </div>
